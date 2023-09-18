1cbe12b91e01f129ea837993d7ac8e60dc70db24
unknown <guillaumedhoirpro@gmail.com>
2023-09-18 12:12:49 +0200

    commit

---
+++ les_listes/main.py
@@ -0,0 +1,27 @@
+#ex01
+L1 = ["Guillaume", "Celine", "Artine"]
+
+print(L1)
+print(len(L1))
+for e in L1:
+    print(e)
+
+#ex02
+L2 = ["Guillaume", 25, 12.5, True]
+print(L2)
+L2.append("Celine")
+print(L2)
+
+#ex03
+#def remplace(lst, str, str_update):
+#    for i in range(len(lst)):
+#        if lst[i] == str:
+#            lst[i] = str_update
+#    return lst
+
+L3 = ["Voiture", "Moto", "Velo"]
+print(L3)
+#remplace(L3, "Moto", "Scooter")
+L3.pop(1)
+L3.insert(1, "Scooter")
+print(L3)
---
+++ les_variables/main.py
@@ -0,0 +1,18 @@
+#ex01
+nbr1 = 23
+nbr2 = 2
+
+#ex02
+print(nbr1 + nbr2)
+
+#ex03
+print(nbr1 - nbr2)
+
+#ex04
+print(nbr1 * nbr2)
+
+#ex05
+print(nbr1 / nbr2)
+
+#ex06
+print(nbr1 % nbr2)
