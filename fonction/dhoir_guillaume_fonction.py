0bbc7252f8a9c257dd22d3d9bbcf0ca42e8592fc
unknown <guillaumedhoirpro@gmail.com>
2023-09-20 09:54:02 +0200

    *

--- fonction/dhoir_guillaume_fonction.py
+++ fonction/dhoir_guillaume_fonction.py
@@ -1,20 +1,24 @@
+# ex01
 def addition(nbr1, nbr2):
     return (nbr1 + nbr2)
 
+# print(addition(1, 9))
+# ex02
 def soustraction(nbr1, nbr2):
     return (nbr1 - nbr2)
 
+# print(soustraction(4, 2))
+# ex03
 def multiplication(nbr1, nbr2):
     return (nbr1 * nbr2)
 
+# print(multiplication(5, 2))
+# ex04
 def division(nbr1, nbr2):
     return (nbr1 / nbr2)
 
-# print(addition(1, 9))
-# print(soustraction(4, 2))
-# print(multiplication(5, 2))
 # print(division(10, 5))
-
+# ex05
 def calcul():
     nbr1 = int(input("\nEntrez un chiffre >>> "))
     symbol = str(input("Choisissez l'une de ces operation (+, -, *, /) >>> "))
@@ -31,7 +35,7 @@ def calcul():
         print("ERROR")
 
 # calcul()
-
+# ex06
 def capitalize_bis(strg):
     lst = []
     for e in strg:
@@ -45,7 +49,7 @@ def capitalize_bis(strg):
 
 
 # print(capitalize_bis("gUILLAUME"))
-
+# ex07
 def password():
     user_password = str(input("\nMot de passe >>> "))
     if user_password != "password":
